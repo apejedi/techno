fd477615fa3f4d60e0f73beaef94253356d6aea3
apejedi <Jaideep.Umraiya@experian.com>
2017-06-26 14:42:09 -0500

    ascdv

--- src/techno/grid.clj
+++ src/techno/grid.clj
@@ -1,22 +1,22 @@
 (ns techno.grid
-  ;; (:use [techno.samples :only [drum-kits]]
-  ;;       [overtone.sc.trig]
-  ;;       [overtone.sc.node]
-  ;;       [overtone.music.pitch]
-  ;;       [overtone.libs.event :only [on-event event]])
+  (:use [techno.samples :only [drum-kits]]
+        [overtone.sc.trig]
+        [overtone.sc.node]
+        [overtone.music.pitch]
+        [overtone.libs.event :only [on-event event]])
   (:require
-   ;[techno.sequencer :as s]
-            [quil.core :as q]
-            [quil.applet :as ap]
-            )
+   [techno.sequencer :as s]
+   [quil.core :as q]
+   [quil.applet :as ap]
+   )
   (:import [java.awt Color]
            [javax.swing JComboBox JTextArea GroupLayout JFrame JTextField JButton]
-;           [controlP5 ControlP5]
+                                        ;           [controlP5 ControlP5]
            (java.awt Dimension Font Color)
            (java.awt.event ActionListener ActionEvent)))
 
 
-
+(declare grid)
 (def state (atom {}))
 
 (defn setup []
@@ -30,15 +30,17 @@
 
 (defn get-color [o]
   (let [p (get @state :pattern {})
-        cur (get @state :cur 1)]
-      (cond
-        (= o cur) [255 255 0]
-        (and (contains? p o) (= o (apply max (keys p))))
-        (if (and (contains? p o) (not (nil? (first (get p o)))))
-          [255 0 255]
-          [255 0 0])
-        (and (contains? p o) (not (nil? (first (get p o))))) [124 252 0]
-        true [220 220 220]))
+        cur (get @state :cur 1)
+        beat (get @state :beat)]
+    (cond
+      (= o beat) [255 69 0]
+      (= o cur) [255 255 0]
+      (and (contains? p o) (= o (apply max (keys p))))
+      (if (and (contains? p o) (not (nil? (first (get p o)))))
+        [255 0 255]
+        [255 0 0])
+      (and (contains? p o) (not (nil? (first (get p o))))) [124 252 0]
+      true [220 220 220]))
   )
 
 (defn draw []
@@ -53,19 +55,38 @@
       ))
   )
 
+(defn draw-beat [beat]
+  (swap! state assoc :beat (s/i-step (* (int beat) (get @state :step))))
+  ;(q/redraw)
+  ;; (ap/with-applet grid
+  ;;   (let [g (.getGraphics grid)
+  ;;         step (get @state :step)
+  ;;         offset (s/i-step (* (int beat) step))
+  ;;         cur (get-in @state [:coord-map offset])
+  ;;         prev (get-in @state [:coord-map (- offset step)])]
+  ;;     (.beginDraw g)
+  ;;     (when (not (nil? cur))
+  ;;       (q/fill 255 69 0)
+  ;;       (apply q/rect (flatten cur)))
+  ;;     (when (not (nil? prev))
+  ;;       (apply q/fill (get-color offset))
+  ;;       (apply q/rect (flatten cur)))
+  ;;     (.endDraw g)
+  ;;     ))
+  )
+
 (defn handle-key []
   (let [key (q/key-as-keyword)
         cur (get @state :cur 1)
         coords (get @state :coord-map {})
         step (get @state :step 0.25)
         bars (get @state :bars 1)
-        i-step #(if (= (mod % (int %)) 0.0) (int %) %)
         new (cond (= :left key) (- cur step)
                   (= :right key) (+ cur step)
                   (= :up key) (- cur bars)
                   (= :down key) (+ cur bars)
                   true cur)
-        new (i-step new)
+        new (s/i-step new)
         new (if (contains? coords new) new cur)]
     (swap! state assoc :cur new)
     (ap/with-applet grid
@@ -73,7 +94,11 @@
     )
   )
 
-(defn mk-grid [bars rows & [step space width height]]
+(defn set-state [keys val]
+  (swap! state assoc-in keys val)
+  )
+
+(defn mk-grid [bars rows sequencer & [step space width height]]
   (let [step (if (nil? step) 0.25 step)
         space (if (nil? space) 10 space)
         width (if (nil? width) 50 width)
@@ -82,23 +107,26 @@
                    [[(+ (* space (+ c 1)) (* width c)) (+ (* space (inc r)) (* height r))]
                     [(* (+ width space) (inc c)) (* (+ space height) (inc r))]])
         offsets (range 1 (* bars rows) step)
-        i-step #(if (= (mod % (int %)) 0.0) (int %) %)
         coords (into {}
                      (mapcat
                       (fn [r]
                         (mapcat
                          (fn [c]
                            (let [a (g-coords r (inc c))
-                                 o (i-step (+ (inc (* c step)) (* r bars)))]
+                                 o (s/i-step (+ (inc (* c step)) (* r bars)))]
                              [[o a]]))
                          (range 0 (* (/ 1 step) bars))))
-                      (range 0 rows)))]
-    (swap! state merge {:coord-map coords :rows rows :bars bars :step step})
+                      (range 0 rows)))
+        uid (get (s/get-sequencer-data sequencer) :uid)]
+    (swap! state merge {:coord-map coords :rows rows :bars bars :step step :sequencer sequencer})
     (q/defsketch grid
       :setup setup
       :draw draw
       :size :fullscreen
       :key-pressed handle-key)
+    ;; (on-latest-trigger
+    ;;  sequencer uid
+    ;;  draw-beat :draw-beat)
     )
   )
 
